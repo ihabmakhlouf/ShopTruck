c19e24a1d2d70d536240bf43ea62beb8f0b9ce46
Makhlouf Ihab <ext-ihab.makhlouf01@chu-lyon.fr>
2026-01-08 16:38:40 +0100

    Ajouter EntityTypeConfiguration

--- ShopTruck.Store.Infrastructure/Data/AppDbContext.cs
+++ ShopTruck.Store.Infrastructure/Data/AppDbContext.cs
@@ -4,4 +4,11 @@ namespace ShopTruck.Store.Infrastructure.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
     public DbSet<Domain.Entities.Store> Stores { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(AppDbContext).Assembly
+        );
+        }
     }
---
+++ ShopTruck.Store.Infrastructure/Persistence/Configurations/StoreConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ShopTruck.Store.Infrastructure.Persistence.Configurations;
+
+public class StoreConfiguration : IEntityTypeConfiguration<Domain.Entities.Store>
+    {
+    public void Configure(EntityTypeBuilder<Domain.Entities.Store> builder)
+        {
+        builder.ToTable("stores");
+
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.OwnsOne(s => s.Address, address =>
+        {
+            address.Property(a => a.Street)
+                .HasColumnName("street")
+                .IsRequired();
+
+            address.Property(a => a.City)
+                .HasColumnName("city")
+                .IsRequired();
+
+            address.Property(a => a.PostalCode)
+                .HasColumnName("postal_code");
+
+            address.Property(a => a.Country)
+                .HasColumnName("country")
+                .IsRequired();
+        });
+        }
+    }
