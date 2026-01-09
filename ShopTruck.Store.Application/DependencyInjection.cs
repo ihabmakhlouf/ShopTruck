d21ea98e6a4c05070e891335f04254f69d623f7a
Makhlouf Ihab <ext-ihab.makhlouf01@chu-lyon.fr>
2026-01-09 14:49:29 +0100

    ajouter validator

--- ShopTruck.Store.Application/DependencyInjection.cs
+++ ShopTruck.Store.Application/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using ShopTruck.Store.Application.Common;
 
 namespace ShopTruck.Store.Application;
 
@@ -8,6 +11,13 @@ public static class DependencyInjection
         {
         var assembly = typeof(DependencyInjection).Assembly;
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        services.AddTransient(
+            typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehavior<,>));
+
         return services;
         }
     }
