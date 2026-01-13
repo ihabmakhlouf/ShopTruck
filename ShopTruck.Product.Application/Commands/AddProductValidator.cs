65976b35a88417a6052f0c776ca9b52b87894220
Makhlouf Ihab <ext-ihab.makhlouf01@chu-lyon.fr>
2026-01-13 09:25:36 +0100

    ajouter validator

---
+++ ShopTruck.Product.Application/Commands/AddProductValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MyApp.Application.Commands;
+
+namespace ShopTruck.Product.Application.Commands;
+
+public class AddProductValidator : AbstractValidator<AddProductCommand>
+    {
+    public AddProductValidator() 
+        {
+        RuleFor(x => x.productDto.Name).NotNull()
+                        .NotEmpty().WithMessage("{PropertyName} is required.")
+                        .MinimumLength(3)
+                        .MaximumLength(250)
+                        .WithMessage("{PropertyName} must be fewer than 250 characters.");
+
+        RuleFor(x => x.productDto.Description).NotNull()
+                             .NotEmpty().WithMessage("{PropertyName} is required.")
+                             .MaximumLength(250);
+
+        RuleFor(x => x.productDto.Price).NotNull()
+                                        .GreaterThan(0);
+        }
+    }
+
