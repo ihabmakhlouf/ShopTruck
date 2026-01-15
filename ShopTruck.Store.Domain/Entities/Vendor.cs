477fb4bd675de80a22c9a278f295dbd16925daf1
Makhlouf Ihab <ext-ihab.makhlouf01@chu-lyon.fr>
2026-01-15 15:45:12 +0100

    ajouter VendorRepository

---
+++ ShopTruck.Store.Domain/Entities/Vendor.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopTruck.Store.Domain.Entities;
+
+public class Vendor
+    {
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string AddressMail { get; set; } = null!;
+    }
+
--- ShopTruck.Store.Infrastructure/Data/AppDbContext.cs
+++ ShopTruck.Store.Infrastructure/Data/AppDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShopTruck.Store.Domain.Entities;
 namespace ShopTruck.Store.Infrastructure.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
     public DbSet<Domain.Entities.Store> Stores { get; set; }
-
+    public DbSet<Vendor> Vendors { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
         modelBuilder.ApplyConfigurationsFromAssembly(
