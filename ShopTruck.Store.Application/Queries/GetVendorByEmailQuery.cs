2d3f337abbb145ba4a156f493e7132d77800ef1a
Makhlouf Ihab <ext-ihab.makhlouf01@chu-lyon.fr>
2026-01-16 10:20:29 +0100

    GetVendorByIdQueryHandler & GetVendorByEmailQueryHandler

--- ShopTruck.Store.Application/Queries/GetVendorByEmailQuery.cs
+++ ShopTruck.Store.Application/Queries/GetVendorByEmailQuery.cs
@@ -1,6 +1,25 @@
-﻿namespace ShopTruck.Store.Application.Queries;
+﻿using MediatR;
+using ShopTruck.Store.Application.Dtos;
+using ShopTruck.Store.Domain.Interfaces;
 
-public record GetVendorByEmailQuery
+namespace ShopTruck.Store.Application.Queries;
+
+public record GetVendorByEmailQuery(string email) : IRequest<VendorDto>;
+
+public class GetVendorByEmailQueryHandler(IVendorRepository vendorRepository) : IRequestHandler<GetVendorByEmailQuery, VendorDto>
     {
+    public async Task<VendorDto> Handle(GetVendorByEmailQuery request, CancellationToken cancellationToken)
+        {
+        var vendor = await vendorRepository.GetByEmailAsync(request.email, cancellationToken);
+        return new VendorDto
+            {
+            Id = vendor.Id,
+            FirstName = vendor.FirstName,
+            LastName = vendor.LastName,
+            AddressMail = vendor.AddressMail,
+            Password = vendor.Password,
+            };
+        }
     }
+
 
--- ShopTruck.Store.Application/Queries/GetVendorByIdQuery.cs
+++ ShopTruck.Store.Application/Queries/GetVendorByIdQuery.cs
@@ -1,6 +1,24 @@
-﻿namespace ShopTruck.Store.Application.Queries;
+﻿using MediatR;
+using ShopTruck.Store.Application.Dtos;
+using ShopTruck.Store.Domain.Interfaces;
 
-public record GetVendorByIdQuery
+namespace ShopTruck.Store.Application.Queries;
+
+public record GetVendorByIdQuery(Guid Guid): IRequest<VendorDto>;
+
+public class GetVendorByIdQueryHandler(IVendorRepository vendorRepository) : IRequestHandler<GetVendorByIdQuery, VendorDto>
     {
+    public async Task<VendorDto> Handle(GetVendorByIdQuery request, CancellationToken cancellationToken)
+        {
+        var vendor = await vendorRepository.GetByIdAsync(request.Guid, cancellationToken);
+        return new VendorDto
+            {
+            Id = vendor.Id,
+            FirstName = vendor.FirstName,
+            LastName = vendor.LastName,
+            AddressMail = vendor.AddressMail,
+            Password = vendor.Password,
+            };
+        }
     }
 
