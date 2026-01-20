71ca4e91b4ef9975168ec28e9f60e49091301631
Makhlouf Ihab <ext-ihab.makhlouf01@chu-lyon.fr>
2026-01-20 09:05:08 +0100

    add method GetProductById

--- ShopTruck.Product.Grpc/Services/ProductService.cs
+++ ShopTruck.Product.Grpc/Services/ProductService.cs
@@ -1,6 +1,30 @@
-﻿namespace ShopTruck.Product.Grpc.Services
+﻿using Grpc.Core;
+using MediatR;
+using ShopTruck.Product.Application.Queries;
+using ShopTruck.Product.Grpc.Protos;
+
+namespace ShopTruck.Product.Grpc.Services
     {
-    public class ProductService
+    public class ProductService(ISender sender) : ProductServiceGrpc.ProductServiceGrpcBase
         {
+
+        public override async Task<GetProductResponse> GetProductById(GetProductRequest request, ServerCallContext context)
+            {
+            if (!Guid.TryParse(request.Id, out var productId))
+                {
+                throw new RpcException(
+                    new Status(StatusCode.InvalidArgument, "Invalid product ID"));
+                }
+            var product = await sender.Send(new GetProductByIdQuery(productId));
+
+            return new GetProductResponse
+                {
+                Id = product.Id.ToString(),
+                Name = product.Name,
+                Description = product.Description,
+                CategoryId = product.CategoryId.ToString(),
+                Price = product.Price
+                };
+            }
         }
     }
